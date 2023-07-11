d32ec3bf8a07f4e958b1370de77c516aa7e5879b
lovleenkaur <lovleenk99@gmail.com>
2023-07-11 10:34:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2 
+
+const random = Math.floor(Math.random() * 1000) +1
+
+const num3 = 4;
+const num4 = 16;
+
+const mod = num3 % num4
+
+const max = (20)
